2704fe213e4a634bba61dfe196820c1d2c908d11
Thanistas <thanithanis171198@gmail.com>
2022-10-25 21:45:13 +0530

    Upload basic nest structure

--- Winston logger/winston_nestjs/src/app.module.ts
+++ Winston logger/winston_nestjs/src/app.module.ts
@@ -1,18 +1,13 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { WinstonModule } from 'nest-winston';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { useLogger } from './utils/userlogger';
 
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://Thanistas:sample@cluster0.m8mer.mongodb.net/Books?retryWrites=true&w=majority'),
-    UserModule,
     WinstonModule.forRoot( useLogger)
-    
   ],
   controllers: [AppController],
   providers: [AppService],
